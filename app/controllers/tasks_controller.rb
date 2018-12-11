504b2686bcd8b80f6b1eddfcecbd61f2086a9e29
Devin Sweeting and maly phongsavanh <maly@>
2018-12-11 11:29:38 -0800

    add CRUD features to Task class

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,17 @@
 class TasksController < ApplicationController
+
+  def index
+    @lists = List.all
+    @task = Task.all
+    render :index
+  end
+
+  def show
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    redirect_to lists_path(@task)
+  end
+
   def new
     @list = List.find(params[:list_id])
     @task = @list.tasks.new
@@ -14,8 +27,29 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to lists_path(@task.list)
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_task_path
+  else
+    render :edit
+  end
+end
+
   private
-    def task_params
-      params.require(:task).permit(:description)
-    end
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
